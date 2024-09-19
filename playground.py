4f1d8bc0f21de3314faac3c97eeded03ae398648
Tuomas V <115335825+tuomascode@users.noreply.github.com>
2024-09-19 15:18:14 +0000

    I just pushed to master

--- playground.py
+++ playground.py
@@ -3,22 +3,21 @@
 
 def this_function_has_a_pompous_name_but_only_adds_to_numbers_together(x, y):
     return x + y
+a = 5
+b = 10
+print(this_function_has_a_pompous_name_but_only_adds_to_numbers_together(a, b)) # should print 15 to your console.
+exit_value = "yes" # Change this to anything else to keep the code running longer
+if exit_value == "yes":
+    exit
 
-print(this_function_has_a_pompous_name_but_only_adds_to_numbers_together(5, 10)) # should print 15 to your console.
 
 # A PLAYGROUNG MASTER TASK:
-#   CREATE A FUNCTION WITH LESS POMPOUS NAME, BUT SUBSTRACTS A NUMBER
-
-
-
+#   CREATE A FUNCTION WITH LESS? POMPOUS NAME, BUT SUBSTRACTS A NUMBER
 test_passed = "test_failed but function ran"
-
 try:
     if substractooo_periotus(10, 5) == 5:
         test_passed = "test_passed"
 except:
     test_passed= "I think you have to write a function called substractooo_periotus"
-
-
 print(f"This test is passed, if the next line reads 'test passed'\n"
       f"{test_passed}")
