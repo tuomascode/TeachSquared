39e536580884df88656461e372183781e5b11996
Tuomas Vuontisjärvi <tuomasv91@hotmail.com>
2024-09-19 18:05:06 +0300

    improve game

--- playground.py
+++ playground.py
@@ -9,11 +9,16 @@ print(this_function_has_a_pompous_name_but_only_adds_to_numbers_together(5, 10))
 # A PLAYGROUNG MASTER TASK:
 #   CREATE A FUNCTION WITH LESS POMPOUS NAME, BUT SUBSTRACTS A NUMBER
 
-test_passed = "test failed"
+
+
+test_passed = "test_failed but function ran"
+
 try:
     if substractooo_periotus(10, 5) == 5:
         test_passed = "test_passed"
-except: pass
+except:
+    test_passed= "I think you have to write a function called substractooo_periotus"
+
 
 print(f"This test is passed, if the next line reads 'test passed'\n"
       f"{test_passed}")
