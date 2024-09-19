4ebbb71098e9de82caa96536cc6a01967bf53ec8
Tuomas Vuontisjärvi <tuomasv91@hotmail.com>
2024-09-19 18:02:33 +0300

    first coding learing fun

---
+++ playground.py
@@ -0,0 +1,19 @@
+# welcome to the playground code. Feel free to do anything here :D But know that other people might help you with your play.
+
+
+def this_function_has_a_pompous_name_but_only_adds_to_numbers_together(x, y):
+    return x + y
+
+print(this_function_has_a_pompous_name_but_only_adds_to_numbers_together(5, 10)) # should print 15 to your console.
+
+# A PLAYGROUNG MASTER TASK:
+#   CREATE A FUNCTION WITH LESS POMPOUS NAME, BUT SUBSTRACTS A NUMBER
+
+test_passed = "test failed"
+try:
+    if substractooo_periotus(10, 5) == 5:
+        test_passed = "test_passed"
+except: pass
+
+print(f"This test is passed, if the next line reads 'test passed'\n"
+      f"{test_passed}")
